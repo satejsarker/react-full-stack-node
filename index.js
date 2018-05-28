6a0a85c66f3fa639e50c84359244f2c4d876c200
satejsarker <keysarker@outlook.com>
2018-05-28 14:59:11 +0600

    auth is activated without database

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 const express = require('express');
 const passport=require('passport');
 const GoogleStrategy=require('passport-google-oauth20').Strategy;
+const key=require('./config/key');
+
+// var knex = require('knex')({
+//   client: 'mysql',
+//   connection: {
+//     host : '127.0.0.1',
+//     user : 'root',
+//     password : '',
+//     database : 'full_stack'
+//   }
+// });
+
+
+// knex.from('noname').select().then((res)=>{
+ 
+//   console.log(res)
+// })
+
 
 const app = express();
 
-passport.use(new GoogleStrategy())
+passport.use(new GoogleStrategy({
+  clientID:key.GoogleClintId,
+  clientSecret:key.GoogleClintSecret,
+  callbackURL:'/auth/google/callback'
+},
+(accessToken, refreshToken, profile, done) =>{
+  console.log('accessToken '+ accessToken);
+  console.log(refreshToken);
+  console.log(profile);
+}
+))
+
+
+app.get('/auth/google/', passport.authenticate('google',{
+  scope:['profile','email']
+}));
+
+app.get('/auth/google/callback',passport.authenticate('google'));
 
 
 app.get('/', (req, res) => {
@@ -17,6 +52,7 @@ app.get("/2nd", (req, res) => {
     "2nd route ": "seting up my node runtime and route "
   });
 });
+
 
 const PORT=process.env.PORT || 4000;
 app.listen(PORT);
